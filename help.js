cdc45c52c35c9ca101ae531eed8e57a24a274b08
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-02-17 17:35:09 +0900

    재밌당 크크

--- help.js
+++ help.js
@@ -1,6 +1,10 @@
 //한 줄 입력 try.txt 지우고 /dev/stdin
 const s = require('fs').readFileSync('try.txt').toString().split(' ');
+const s = require('fs').readFileSync('/dev/stdin').toString().split(' ');
 
 
 //여러 줄 입력 : try.txt 지우고 /dev/stdin
 const s = require('fs').readFileSync('try.txt').toString().split('\n');
+const s = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+/dev/stdin
--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-const s = require('fs').readFileSync('try.txt').toString().split(' ');
+const s = require('fs').readFileSync('try.txt').toString();
+console.log(`${s}??!`);
