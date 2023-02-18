c42f06674ee18e7d260a29ef4cb5d767779c811a
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-02-18 23:34:00 +0900

    가보자!

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const s = require('fs').readFileSync('try.txt').toString().trim();
-console.log(s + "??!");
+const s = require('fs').readFileSync('try.txt').toString().split("\n");
+
+for (let i=1; i<s.length; i++) {
+  let a = s[i].split(" ");
+  let b = BigInt(a[0]);
+  let c = BigInt(a[1]);
+
+  let d = BigInt(1);
+  for (let i=BigInt(1); i <=b; i=i+BigInt(1)) {
+    d= d * c;
+    c= c - BigInt(1);
+  }
+
+  let e = BigInt(1);
+  for (let i=BigInt(1); i <=b; i=i+BigInt(1)) {
+    e *= i;
+    e= e * BigInt(1);
+  }
+  console.log(d/e+"");
+}
---
+++ test2.js
@@ -0,0 +1,4 @@
+let a= "10";
+console.log(a + a);
+let b= +a;
+console.log(b+ b);
