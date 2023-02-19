3df60e31c56e69083f8b5c82700177a809097622
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-02-19 13:11:15 +0900

    아ㄹ고리즘 재밌어

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const s = require('fs').readFileSync('try.txt').toString().split("\n");
-
-for (let i=1; i<s.length; i++) {
-  let a = s[i].split(" ");
-  let b = BigInt(a[0]);
-  let c = BigInt(a[1]);
-
-  let d = BigInt(1);
-  for (let i=BigInt(1); i <=b; i=i+BigInt(1)) {
-    d= d * c;
-    c= c - BigInt(1);
-  }
-
-  let e = BigInt(1);
-  for (let i=BigInt(1); i <=b; i=i+BigInt(1)) {
-    e *= i;
-    e= e * BigInt(1);
-  }
-  console.log(d/e+"");
+const s = require('fs').readFileSync('/dev/stdin').toString();
+let a = "";
+let b = "";
+for (let i = 0; i < parseInt(s); i++) {
+  a += "*";
+  b += `${a}\n`;
 }
+console.log(b);
