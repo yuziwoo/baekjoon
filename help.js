0b63066ba5519b5c4c33fc75c7adcdef9150d22a
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-02-24 19:49:44 +0900

    zz

--- help.js
+++ help.js
@@ -12,5 +12,18 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString();
 
+// readline
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+let input = [];
+rl.on('line', function (line) {
+  input.push(parseInt(line));
+}).on('close', function () {
+  const [x, y] = input;
+  })
 
 /dev/stdin
