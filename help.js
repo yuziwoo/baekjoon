e94b07da59c951e3b760a602d243e1af7e94ef05
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-02-25 18:26:34 +0900

    푸는중 총총

--- help.js
+++ help.js
@@ -1,11 +1,11 @@
 //한 줄 입력 try.txt 지우고 /dev/stdin
-const s = (require('fs').readFileSync('try.txt')+'').split(' ');
-const s = (require('fs').readFileSync('/dev/stdin')+'').split(' ');
+const s = require('fs').readFileSync('try.txt').toString().split(' ');
+const s = require('fs').readFileSync('/dev/stdin').toString().split(' ');
 
 
 //여러 줄 입력 : try.txt 지우고 /dev/stdin
-const s = (require('fs').readFileSync('try.txt')+'').split('\n');
-const s = (require('fs').readFileSync('/dev/stdin')+'').split('\n');
+const s = require('fs').readFileSync('try.txt').toString().split('\n');
+const s = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 // 이렇게 해도 됨
 const fs = require("fs");
