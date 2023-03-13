6e34491d3074995d1eeca3c1806caed366feda15
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-03-13 15:14:19 +0900

    백준 기본 텍스트 수정

--- help.js
+++ help.js
@@ -1,11 +1,11 @@
 //한 줄 입력 try.txt 지우고 /dev/stdin
 const s = require('fs').readFileSync('try.txt').toString().split(' ');
-const s = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const s = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 
 
 //여러 줄 입력 : try.txt 지우고 /dev/stdin
 const s = require('fs').readFileSync('try.txt').toString().split('\n');
-const s = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const s = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 // 이렇게 해도 됨
 const fs = require("fs");
